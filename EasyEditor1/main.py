bdac8ad6d17827a8a7f9099ac3807725d54def81
MaksymFed <maks@gmail.com>
2024-03-03 13:08:13 +0200

    my doddaly choosing directory

--- EasyEditor1/main.py
+++ EasyEditor1/main.py
@@ -1,12 +1,42 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from ui import Ui_MainWindow
+import os
 
 app = QApplication([])
 win = QMainWindow()
 
 ui = Ui_MainWindow()
 ui.setupUi(win)
+#-----------------------------
+workdir = ""
+
+def chooseWorkDir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+    print(workdir)
 
 
+def filter(files, extensions):
+    graphical_files = []
+
+    for file in files:
+        for ext in extensions:
+            if file.endswith(ext):
+               graphical_files.append(file)
+
+    return graphical_files
+
+def showFilenamesList():
+    chooseWorkDir()
+    extensions = [".png", ".jpg", ".jpeg", ".bmp", ".gif"]
+    
+    filenames = os.listdir(workdir)
+    filenames = filter(filenames, extensions)
+    print(filenames)
+    ui.files_list.clear()
+    ui.files_list.addItems(filenames)
+ui.dir_btn.clicked.connect(showFilenamesList)
+
+#-------------------------------
 win.show()
 app.exec()
